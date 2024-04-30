98e53877665dd5c873bd1268d0be94b316802762
Hamida REBAI <didourebai@gmail.com>
2024-04-30 08:16:10 -0400

    Add project CHAPITRE11

---
+++ CHAPITRE11_VIDEO08_01/EvaluationProduit/EvaluationProduit.MVC/Services/IProduitService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using EvaluationProduit.MVC.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace EvaluationProduit.MVC.Services
+{
+    public interface IProduitService
+    {
+        IList<ProduitModel> ProduitModels { get; set; }
+        public float RecupererlaMoyenneEvaluation(int evaluation1, int evaluation2);
+        public PhotoModel ChargerFichier(IFormFile photoFile, string webRootPath);
+    }
+}
